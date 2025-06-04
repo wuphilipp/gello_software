0778b1d73df9b3710248a87abbc28eb96b10bdb3
Jeffrey Liu <jliueducation@gmail.com>
2025-06-04 15:23:03 -0700

    formatting fixes

--- scripts/yam_blocks_play.py
+++ scripts/yam_blocks_play.py
@@ -4,9 +4,6 @@ import numpy as np
 import tyro
 from dm_control import composer, viewer
 
-from typing import List, Optional
-
-
 from gello.agents.gello_agent import DynamixelRobotConfig
 from gello.dm_control_tasks.arms.yam import YAM
 from gello.dm_control_tasks.manipulation.arenas.floors import Floor
@@ -18,13 +15,15 @@ class Args:
     use_gello: bool = True
     control_rate_hz: float = 100.0
 
+
 config = DynamixelRobotConfig(
     joint_ids=(1, 2, 3, 4, 5, 6),
     joint_offsets=(0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
     joint_signs=(-1, 1, 1, 1, -1, -1),
-    gripper_config=(7, 0.0, 0.5)
+    gripper_config=(7, 0.0, 0.5),
 )
-    
+
+
 def main(args: Args) -> None:
     reset_joints_left = np.deg2rad([90, -90, -90, -90, 90, 0, 0])
     robot = YAM()
@@ -34,9 +33,9 @@ def main(args: Args) -> None:
     action_space = env.action_spec()
     if args.use_gello:
         gello = config.make_robot(
-            port="/dev/ttyUSB0", 
+            port="/dev/ttyUSB0",
             start_joints=reset_joints_left,
-            baudrate=1000000  # Use 1Mbps as detected
+            baudrate=1000000,  # feetech gellos want 1M baudrate
         )
 
     def policy(timestep) -> np.ndarray:
@@ -49,6 +48,7 @@ def main(args: Args) -> None:
         return np.random.uniform(action_space.minimum, action_space.maximum)
 
     viewer.launch(env, policy=policy)
+
 
 if __name__ == "__main__":
     main(tyro.cli(Args))
