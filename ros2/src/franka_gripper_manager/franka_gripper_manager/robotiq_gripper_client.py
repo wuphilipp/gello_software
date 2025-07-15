081341737dd3aa6e7095df077e5907f76d186e03
Andreas Gassner <andreas.gassner@franka.de>
2025-07-15 13:08:21 +0000

    add: ros2 franka multi arm support using namespaces with robotiq

--- ros2/src/franka_gripper_manager/franka_gripper_manager/robotiq_gripper_client.py
+++ ros2/src/franka_gripper_manager/franka_gripper_manager/robotiq_gripper_client.py
@@ -4,7 +4,7 @@ from control_msgs.action import GripperCommand
 from rclpy.action import ActionClient
 from std_msgs.msg import Float32
 
-DEFAULT_GRIPPER_COMMAND_TOPIC = "/gripper/gripper_client/target_gripper_width_percent"
+DEFAULT_GRIPPER_COMMAND_TOPIC = "/gripper_client/target_gripper_width_percent"
 DEFAULT_MOVE_ACTION_TOPIC = "/robotiq_gripper_controller/gripper_cmd"
 
 
@@ -14,7 +14,7 @@ class RobotiqGripperClient(Node):
         self.declare_parameter("namespace", "")
         self.get_logger().info("Starting Robotiq Gripper Client")
         self.namespace = self.get_parameter("namespace").get_parameter_value().string_value
-        self.gripper_state_callback = self.create_subscription(
+        self.gripper_state_sub = self.create_subscription(
             Float32,
             self.namespace + DEFAULT_GRIPPER_COMMAND_TOPIC,
             self.gripper_state_callback,
@@ -32,6 +32,27 @@ class RobotiqGripperClient(Node):
         if abs(gripper_target_position - self.last_width) < 0.02:
             return
         self.send_gripper_command(gripper_target_position)
+
+    def send_gripper_command(self, gripper_position):
+        self.last_width=gripper_position
+        goal_msg = GripperCommand.Goal()
+        goal_msg.command.position = 1-gripper_position # Convert to 0-1 range
+        goal_msg.command.max_effort = 1.0
+        self.future=self.action_client.send_goal_async(goal_msg)
+        self.future.add_done_callback(self.gripper_response_callback)
+
+
+    def gripper_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            raise RuntimeError(f'Goal rejected with status: {goal_handle.status}')
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info('Result: {0}'.format(result))
 
 
 def main(args=None):
--- ros2/src/franka_gripper_manager/launch/robotiq_gripper_controller_client.launch.py
+++ ros2/src/franka_gripper_manager/launch/robotiq_gripper_controller_client.launch.py
@@ -11,30 +11,25 @@ from launch_ros.substitutions import FindPackageShare
 def load_yaml(file_path):
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"File not found: {file_path}")
-    with open(file_path, "r") as file:
+    with open(file_path, 'r') as file:
         return yaml.safe_load(file)
 
-
 def generate_robot_nodes(context):
-    config_file = LaunchConfiguration("robotiq_config_file").perform(context)
+    config_file = LaunchConfiguration('robotiq_config_file').perform(context)
     configs = load_yaml(config_file)
     nodes = []
     for item_name, config in configs.items():
-        namespace = config["namespace"]
+        namespace = config['namespace']
         nodes.append(
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
-                    PathJoinSubstitution(
-                        [
-                            FindPackageShare("franka_gripper_manager"),
-                            "launch",
-                            "robotiq_gripper_controller_client.launch.py",
-                        ]
-                    )
+                    PathJoinSubstitution([
+                        FindPackageShare('franka_gripper_manager'), 'launch', 'robotiq.launch.py'
+                    ])
                 ),
                 launch_arguments={
-                    "namespace": namespace + "/gripper",
-                    "com_port": str(config["com_port"]),
+                    'namespace': namespace ,
+                    'com_port': str(config['com_port']),
                 }.items(),
             )
         )
@@ -42,15 +37,13 @@ def generate_robot_nodes(context):
 
 
 def generate_launch_description():
-    return LaunchDescription(
-        [
-            DeclareLaunchArgument(
-                "robotiq_config_file",
-                default_value=PathJoinSubstitution(
-                    [FindPackageShare("franka_gripper_manager"), "config", "robotiq-config.yaml"]
-                ),
-                description="Path to the robot configuration file to load",
-            ),
-            OpaqueFunction(function=generate_robot_nodes),
-        ]
-    )
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'robotiq_config_file',
+            default_value=PathJoinSubstitution([
+                FindPackageShare('franka_gripper_manager'), 'config', 'robotiq-config.yaml'
+            ]),
+            description='Path to the robot configuration file to load',
+        ),
+        OpaqueFunction(function=generate_robot_nodes),
+    ])
