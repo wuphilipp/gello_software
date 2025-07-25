fixed joint sign inconsistency in gello_agent.py

gello/agents/gello_agent.py
@@ -70,7 +70,7 @@ PORT_CONFIG_MAP: Dict[str, DynamixelRobotConfig] = {
             5 * np.pi / 3,
             2 * np.pi / 2,
         ],
-        joint_signs=(1, 1, -1, -1, 1, 1),
+        joint_signs=(1, -1, -1, -1, 1, 1),
         gripper_config=(
             7,
             -30,