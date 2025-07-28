6eb82eb975a010e25f98037a4e2acb303905689b
Jeffrey Liu <jliueducation@gmail.com>
2025-07-28 16:44:18 -0700

    removed motor timeout comments from yam.py

--- gello/robots/yam.py
+++ gello/robots/yam.py
@@ -8,10 +8,9 @@ from gello.robots.robot import Robot
 class YAMRobot(Robot):
     """A class representing a simulated YAM robot."""
 
-    def __init__(self, channel="can0"):  # , motor_timeout_enabled=True
+    def __init__(self, channel="can0"):
         from i2rt.robots.get_robot import get_yam_robot
 
-        # self.robot = get_yam_robot(channel=channel, motor_timeout_enabled=motor_timeout_enabled)
         self.robot = get_yam_robot(channel=channel)
 
         # YAM has 7 joints (6 arm joints + 1 gripper)
