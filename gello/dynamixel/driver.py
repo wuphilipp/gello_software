d5f481d3d7d7e9e8327ad401cbd1619b7682a138
Hardik Maheshwari <hardikmahesh21@gmail.com>
2025-09-05 13:20:55 -0700

    remove top comments from dynamixel driver.py

--- gello/dynamixel/driver.py
+++ gello/dynamixel/driver.py
@@ -1,26 +1,3 @@
-# ---------------------------------------------------------------------------
-# FACTR: Force-Attending Curriculum Training for Contact-Rich Policy Learning
-# https://arxiv.org/abs/2502.17432
-# Copyright (c) 2025 Jason Jingzhou Liu and Yulong Li
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ---------------------------------------------------------------------------
-# Based on:
-# https://github.com/wuphilipp/gello_software/blob/main/gello/dynamixel/driver.py
-# ---------------------------------------------------------------------------
-
-
-import os
 import subprocess
 import time
 from threading import Event, Lock, Thread
